c87219e4e095c58e2a8465be9bd9ec9d87b911f7
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2023-01-18 16:04:17 +0100

    Update fraction.py

--- tp07/fraction.py
+++ tp07/fraction.py
@@ -1,5 +1,4 @@
-# Python 3.9
-# UTF-8
+import pydoc
 
 
 class Fraction:
@@ -12,16 +11,13 @@ class Fraction:
 
     def __init__(self, num=0, den=1):
         """This builds a fraction based on some numerator and denominator.
-        PRE : num is an integer by default 0, den is an integer different from 0 and by default 1
-        POST : /
-        RAISES : ZeroDivisionError id den == 0
-        """
-        if den == 0:
-            raise ZeroDivisionError
-        else:
-            self.__num = num
-            self.__den = den
 
+        PRE : num is an integer and by default 0. den is an integer and by default 1.
+        POST : /
+        RAISES : ZeroDivisionError if den ==0
+        """
+        self.__num = num
+        self.__den = den
 
     @property
     def numerator(self):
@@ -34,31 +30,26 @@ class Fraction:
     # ------------------ Textual representations ------------------
 
     def __str__(self):
-        """Return a textual representation of the most simplified form of the fraction
+        """Return a textual representation of the reduced form of the fraction
+
         PRE : /
-        POST : Return the most simplified form of the fraction
+        POST : Return num/den with 2 decimals.
         """
-        n = self.__num
-        d = self.__den
-        i = 2
-        while i < min(n, d) + 1:
-            if n % i == 0 and d % i == 0:
-                n = n // i
-                d = d // i
-            else:
-                i += 1
-        return str(n)+"/"+str(d)
+        return str(self.__num)+"/"+str(self.__den)
 
     def as_mixed_number(self):
-        """Return a textual representation of the reduced form of the fraction as a mixed number
+        """Return a textual representation of the simplified form of the fraction as a mixed number
 
         A mixed number is the sum of an integer and a proper fraction
 
         PRE : /
-        POST : Return an integer + la Fraction
+        POST : Return num/den + an integer
         """
-        return (self.__num/self.__den)+2
-
+        try:
+            return str(int(self.__num/self.__den))+" + "+str(self.__num-(int(self.__num/self.__den)*self.__den))+"/"\
+                   + str(self.__den)
+        except ZeroDivisionError:
+            return 0
     # ------------------ Operators overloading ------------------
 
     def __add__(self, other):
@@ -67,70 +58,90 @@ class Fraction:
          PRE : other is an integer
          POST : Return num/den + other
          """
-        return round(((self.__num/self.__den)+other), 2)
+        try:
+            return (self.__num/self.__den)+other
+        except ZeroDivisionError:
+            return 0
 
     def __sub__(self, other):
         """Overloading of the - operator for fractions
 
         PRE : other is an integer
-         POST : Return num/den - other
+        POST : Return num/den - other
         """
-        return round(((self.__num/self.__den)-other), 2)
+        try:
+            return (self.__num/self.__den)-other
+        except ZeroDivisionError:
+            return 0
 
     def __mul__(self, other):
         """Overloading of the * operator for fractions
 
         PRE : other is an integer
-         POST : Return num/den * other
+        POST : Return num/den * other
         """
-        return round(((self.__num/self.__den)*other), 2)
+        try:
+            return (self.__num/self.__den)*other
+        except ZeroDivisionError:
+            return 0
 
     def __truediv__(self, other):
         """Overloading of the / operator for fractions
 
         PRE : other is an integer
-         POST : Return (num/den)/other
-        RAISES : ZeroDivisionError if other == 0
+        POST : Return num/den / other
         """
-        if other == 0:
-            raise ZeroDivisionError
-        else:
-            return round(((self.__num/self.__den)/other), 2)
+        try:
+            return (self.__num/self.__den)/other
+        except ZeroDivisionError:
+            return 0
 
     def __pow__(self, other):
         """Overloading of the ** operator for fractions
 
         PRE : other is an integer
-         POST : Return (num/den) ** other
+        POST : Return num/den ** other
         """
-        return round(((self.__num/self.__den)**other), 4)
+        try:
+            return (self.__num/self.__den)**other
+        except ZeroDivisionError:
+            return 0
 
     def __eq__(self, other):
-        """Overloading of the == operator for fractions
+        """
+        Overloading of the == operator for fractions
 
         PRE : other is an integer
-         POST : Return True if num/den == other else False
+        POST : Return True si other=num/den sinon False
 
         """
-        return round((self.__num/self.__den), 2) == other
+        try:
+            return (self.__num/self.__den) == other
+        except ZeroDivisionError:
+            return 0
 
     def __float__(self):
-        """Returns the decimal value of the fraction
+        """
+        Returns the decimal value of the fraction
 
         PRE : /
         POST : Return the decimal value of the fraction
         """
-        return self.__num/self.__den
+        try:
+            return self.__num/self.__den
+        except ZeroDivisionError:
+            return 0
 
     # TODO : [BONUS] You can overload other operators if you wish (ex : <, >, ...)
 
     # ------------------ Properties checking ------------------
 
     def is_zero(self):
-        """Check if a fraction's value is 0
+        """
+        Check if a fraction's value is 0
 
         PRE : /
-        POST : Return True if num == 0 else False
+        POST : Return True if num/den=0 else False
         """
         if self.__num == 0:
             return True
@@ -138,62 +149,83 @@ class Fraction:
             return False
 
     def is_integer(self):
-        """Check if a fraction is integer (ex : 8/4, 3, 2/2, ...)
+        """
+        Check if a fraction is integer (ex : 8/4, 3, 2/2, ...)
 
         PRE : /
-        POST : Return True si num/den == integer else False
+        POST : Return True if num/den gives un integer else False
         """
-        if self.__num / self.__den % 1 == 0:
-            return True
-        else:
-            return False
+
+        try:
+            if self.__num/self.__den % 1 == 0:
+                return True
+            else:
+                return False
+        except ZeroDivisionError:
+            return 0
 
     def is_proper(self):
-        """Check if the absolute value of the fraction is < 1
+        """
+        Check if the absolute value of the fraction is < 1
 
         PRE : /
-        POST : Return True if |num/den| < 1 else False
+        POST : Return True if |num/den| <1 else False
         """
-        if abs(self.__num/self.__den) < 1:
-            return True
-        else:
-            return False
+        try:
+            if abs(self.__num/self.__den) < 1:
+                return True
+            else:
+                return False
+        except ZeroDivisionError:
+            return 0
 
     def is_unit(self):
-        """Check if a fraction's numerator is 1 in its reduced form
+        """
+        Check if a fraction's numerator is 1 in its reduced form
 
         PRE : /
-        POST : Return True si num == 1 while the fraction is simplified else it returns False
+        POST : Return True if after being simplified, num == 1 else False
         """
-        n = self.__num
-        d = self.__den
-        i = 2
-        while i < min(n, d) + 1:
-            if n % i == 0 and d % i == 0:
-                n = n // i
-                d = d // i
+        try:
+            if int(self.__num/pgcd(self.__num, self.__den)) == 1:
+                return True
             else:
-                i += 1
-        if n == 1:
-            return True
-        else:
-            return False
+                return False
+        except ZeroDivisionError:
+            return 0
 
     def is_adjacent_to(self, other):
-        """Check if two fractions differ by a unit fraction
+        """
+        Check if two fractions differ by a unit fraction (1/x)
 
         Two fractions are adjacents if the absolute value of the difference them is a unit fraction
 
-        PRE : /
-        POST : Return True if |(num/den)-other|  = 1/x else False
-        RAISES : ZeroDivisionError if other == 0
+        PRE : other is a float
+        POST : Return True if |(num/den)-other| = 1/x else False
         """
-        if other == 0:
-            raise ZeroDivisionError
+        print(other, pgcd(int(other*10), 10), self.__den)
+        if 1/other > self.__den:
+            for i in range(1, int(1/other)):
+                if 1/i == other:
+                    print("Trouvé "+str(i))
+            print(True)
+
+
+
+def pgcd(a, b):
+    """
+    Calculus of the greatest common divisor with the Euclide algorithm
+    :param a: First value , must be greater than B
+    :param b: Second value
+    :return: The GCD
+    """
+    r = -1
+    while r != 0:
+        if b != 0:
+            r = a % b
         else:
-            max_range = int(min((self.__num/self.__den), other)**-1)
-            response = False
-            for i in range(1, max_range+1):
-                if abs((self.__num/self.__den)-other) == 1/i:
-                    response = True
-            return response
+            r = a
+        a, b = b, r
+    return a
+
+# pydoc.help()
